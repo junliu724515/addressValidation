69c5df07c8cb2a41a486d7e461eec390c3c3fe53
Jun Liu <jun.liu@salesforce.com>
2016-03-30 11:02:20 +1100

    added missing static resources

--- scripts/target_env.sh
+++ scripts/target_env.sh
@@ -2,5 +2,5 @@
 # Enter your target org crendentials to deploy here
 #
 export sfdc_username=junliu@mobile.demo
-export sfdc_pass_and_token=demo3456
+export sfdc_pass_and_token=demo1234
 export sfdc_server_url=https://login.salesforce.com
