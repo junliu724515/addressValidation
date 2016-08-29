c2d24d2ca373eb244183eaeeb211fca0b23bfbc3
Jun Liu <jun.liu@salesforce.com>
2016-08-29 17:20:46 +1000

    change keyup event to onblue event

--- scripts/target_env.sh
+++ scripts/target_env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Enter your target org crendentials to deploy here
 #
-export sfdc_username=junliu@mobile.demo
+export sfdc_username=junliu@july2016.demo
 export sfdc_pass_and_token=demo1234
 export sfdc_server_url=https://login.salesforce.com
